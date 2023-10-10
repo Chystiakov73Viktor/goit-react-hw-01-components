b7ebed3f34c93ad1c1826808829072d3331bd5ec
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-10-10 21:02:45 +0300

    created transaction

---
+++ src/components/Transaction/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td className={css.td}>{item.type}</td>
+            <td className={css.td}>{item.amount}</td>
+            <td className={css.td}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+// {
+//     "id": "a30f821b-4d25-4ff0-abdd-e340b3f0dd2b",
+//     "type": "payment",
+//     "amount": "686.50",
+//     "currency": "WST"
+//   },
