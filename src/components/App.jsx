f15d295798359a9cbdc07e7cb0a442caafd564a0
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-10-10 21:14:55 +0300

    created component App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-import { Profile } from "./Profile/Profile";
-import user from '../user.json';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './Friends/FriendList';
+import { TransactionHistory } from './Transaction/TransactionHistory';
+
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './Friends/friends.json';
+import transactions from './Transaction/transactions.json';
 
 export const App = () => {
   return (
-    <div>
-      <Profile 
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
-    </div>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
